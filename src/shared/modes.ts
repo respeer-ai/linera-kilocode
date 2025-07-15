1c65a403715ff3e2e8b75d42e038c90f1206ca31
Zhao KK <kikakkz@hotmail.com>
2025-07-15 22:34:45 +0800

    Correct favicon

--- src/shared/modes.ts
+++ src/shared/modes.ts
@@ -118,7 +118,7 @@ Prefer using alloc, core, and data structures that are known to compile to WASM 
 - On **Windows**, use the https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe to download rustup-init.exe then execute
 - On **macOS** or **Linux**, run: curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-The following file templates are examples only. They are not meant to be copied as-is. When generating code, always adapt these templates to match the user's intent, functionality, and requirements.
+The following file templates are examples only. They are not meant to be copied as-is. When generating code, always adapt these templates to match the user's intent, functionality, and requirements. If you need to create icon, just create simple svg.
 
 📁 project_name/src/lib.rs (Template)
 \`\`\`rust
